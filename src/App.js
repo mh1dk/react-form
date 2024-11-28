d63277952ea729be749bc7293b7ed9525f303fa4
Mahdi Karimi <mhdksp566@gmail.com>
2024-11-28 15:40:01 +0330

    table updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import './App.css';
 import Table from './components/Table';
+import Form from './components/Form';
 
 
 function App() {
@@ -16,38 +17,14 @@ function App() {
       <div className='row vh-100 justify-content-center align-items-center '>
         <div className='col-lg-6'>
 
-          {/* <table class="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colspan="2">Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table> */}
+          
 
             <Table data={staff} />
 
+        </div>
+
+        <div className='col-lg-6 p-5 border-start'>
+          <Form data={staff} updater = {setStaff} />
         </div>
       </div>
     </div>
